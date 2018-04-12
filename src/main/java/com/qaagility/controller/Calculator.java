eab3af31ffbf981564df2af9a4b7a8ba637e38ea
vmalkauthekar <38283717+vmalkauthekar@users.noreply.github.com>
2018-04-12 15:42:05 +0530

    Update Calculator.java

--- src/main/java/com/qaagility/controller/Calculator.java
+++ src/main/java/com/qaagility/controller/Calculator.java
@@ -1,10 +1,22 @@
 package com.qaagility.controller;
 
 public class Calculator {
+  int x, y, result;
 
-  public int add() {
-    return 3 + 6;
+  public int add(int x, int y) {
+    System.out.println("The real add method is called."); 	
+    result = x + y;
+    return result;
+  
+  }
+
+  //method tobe implemented
+  public int mul(int x, int y) {
+    System.out.println("The real mul method is called."); 	
+    //Implementation of the method is pending
+    return result;
   
   }
   
-  }
+  
+}
